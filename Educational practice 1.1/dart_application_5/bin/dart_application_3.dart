03ef9e60844e90b1c16af9cc1460c7f1141ff3c7
Akamychev <114072502+Akamychev@users.noreply.github.com>
2022-11-12 09:20:33 +0700

    Delete Educational practice 1.1 directory

--- Educational practice 1.1/dart_application_5/bin/dart_application_3.dart
+++
@@ -1,9 +0,0 @@
-import "dart:io";
-import 'dart:math';
-
-void main() {
-  String text = stdin.readLineSync()!;
-  List<String> a = text.split(" ");
-  print("Количество слов = ${a.length}");
-  print("Start $text End");
-}
